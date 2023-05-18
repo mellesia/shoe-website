41345a67e0c404acbc0d3a8ff39040005b8130c5
mellesia maged <Mellesiamaged@gmail.com>
2023-05-18 12:00:16 +0300

    update
    category page

--- login.php
+++ login.php
@@ -5,10 +5,27 @@
 ?>
 <!DOCTYPE html>
 <html>
-    <head>
-        <title>Welcome to Trendy shoe</title>
-        <link rel="stylesheet" type="text/css" href="css/style.css" />
-    </head>
+<head>
+   <!-- Basic -->
+   <meta charset="utf-8" />
+   <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+   <!-- Mobile Metas -->
+   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+   <!-- Site Metas -->
+   <meta name="keywords" content="" />
+   <meta name="description" content="" />
+   <meta name="author" content="" />
+   <link rel="shortcut icon" href="images/favicon.png" type="">
+   <title>shoes website</title>
+   <!-- bootstrap core css -->
+   <link rel="stylesheet" type="text/css" href="css/bootstrap.css" />
+   <!-- font awesome style -->
+   <link href="css/font-awesome.min.css" rel="stylesheet" />
+   <!-- Custom styles for this template -->
+   <link href="css/style.css" rel="stylesheet" />
+   <!-- responsive style -->
+   <link href="css/responsive.css" rel="stylesheet" />
+</head>
     <body>
         
         <div class="signInContainer">
